ad6c72cd3a33c482e89b223f4573942f9a5ff0d9
Yui Nakorn <yuinakorn@hotmail.com>
2022-12-22 16:08:17 +0700

    update smog r1

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def root():
 @app.post("/{api_name}", status_code=status.HTTP_200_OK, tags=["Create"])
 async def api(request: Request, api_name: str):  # api_name is parameter to select database
     # test api r1
-    if api_name == "smog_r1":
+    if api_name == "send_smog_r1":
         url = "https://smog-epinorth.chiangmaihealth.go.th/web/index.php?r=upload/json"
 
         json_data = await request.json()
@@ -62,92 +62,91 @@ async def api(request: Request, api_name: str):  # api_name is parameter to sele
         return {"message": response}
     # end test api r1
 
+    else:
+        json_data = await request.json()
+        i = 0
+        hoscode = str(json_data["hcode"])
+        table_name = json_data["table"]
+        method = json_data["method"]
 
+        connection = get_connection(api_name)
 
-    json_data = await request.json()
-    i = 0
-    hoscode = str(json_data["hcode"])
-    table_name = json_data["table"]
-    method = json_data["method"]
+        if connection is None:
+            raise HTTPException(status_code=404, detail="API Not Found")
 
-    connection = get_connection(api_name)
-
-    if connection is None:
-        raise HTTPException(status_code=404, detail="API Not Found")
-
-    if method == "deleteinsert":
-        sql_delete = "DELETE FROM " + table_name + " WHERE HOSPCODE = " + hoscode + ";"
-        print(sql_delete)
-        with connection.cursor() as cursor:
-            cursor.execute(sql_delete)
-            connection.commit()
-        print({"detail": "Delete Success"})
-
-    data = json_data["data"]
-
-    for item in data:
-        dictionary = item
-        print(dictionary)
-
-        headers = []
-        values = []
-        for key in dictionary:
-            value = dictionary[key]
-            headers.append(key)
-            values.append(value)
-
-        # convert list to string with double quote
-        headers_str = ','.join(headers)
-        values_str = '"' + '","'.join(map(str, values)) + '"'  # map(str, values) convert int to str
-        # replace "None" to NULL
-        # values_str = values_str.replace('\"None\"', 'NULL')
-
-        if method == "replace":
-            # sql = "REPLACE INTO " + table_name + " (" + headers_str + ") VALUES (" + values_str + ");"
-            columns = ', '.join("`" + str(x).replace('/', '_') + "`" for x in dictionary.keys())
-            values_str = ', '.join("'" + str(x).replace('/', '_') + "'" for x in dictionary.values())
-            values = values_str.replace('\"None\"', 'NULL')
-            sql = "REPLACE INTO %s (%s) VALUES (%s);" % (table_name, columns, values)
-            print(sql)
-        else:
-            sql = "INSERT INTO " + table_name + " (" + headers_str + ") VALUES (" + values_str + ");"
-
-        i += 1
-
-        try:
+        if method == "deleteinsert":
+            sql_delete = "DELETE FROM " + table_name + " WHERE HOSPCODE = " + hoscode + ";"
+            print(sql_delete)
             with connection.cursor() as cursor:
-                # cursor.execute(sql)
-                cursor.execute(sql)
-                connection.commit()  # commit the changes
-        except Exception as e:
-            print(f'This is error: {e}')
-            error = str(e)
-            connection.rollback()  # rollback if any exception occurred (optional)
-            # write log to file
-            with open('log.txt', 'a') as f:
-                current_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                f.write(f'{current_date} {error} \n')
-            return {"detail": error}
+                cursor.execute(sql_delete)
+                connection.commit()
+            print({"detail": "Delete Success"})
 
-    with open('log.txt', 'a') as f:
-        current_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        f.write(f'{current_date} Insert {i} rows into {hoscode} success \n')
+        data = json_data["data"]
 
-    connection.close()
+        for item in data:
+            dictionary = item
+            print(dictionary)
 
-    # url = "https://notify-api.line.me/api/notify"
-    #
-    # message = "API " + api_name + " end process"
+            headers = []
+            values = []
+            for key in dictionary:
+                value = dictionary[key]
+                headers.append(key)
+                values.append(value)
 
-    # payload = f'message={message}'
-    # headers = {
-    #     'Authorization': 'Bearer ' + config_env["LINE_TOKEN"],
-    #     'Content-Type': 'application/x-www-form-urlencoded'
-    # }
-    #
-    # requests.request("POST", url, headers=headers, data=payload)
+            # convert list to string with double quote
+            headers_str = ','.join(headers)
+            values_str = '"' + '","'.join(map(str, values)) + '"'  # map(str, values) convert int to str
+            # replace "None" to NULL
+            # values_str = values_str.replace('\"None\"', 'NULL')
 
-    return {"detail": "Insert " + str(i) + " rows into " + hoscode + " success"}
+            if method == "replace":
+                # sql = "REPLACE INTO " + table_name + " (" + headers_str + ") VALUES (" + values_str + ");"
+                columns = ', '.join("`" + str(x).replace('/', '_') + "`" for x in dictionary.keys())
+                values_str = ', '.join("'" + str(x).replace('/', '_') + "'" for x in dictionary.values())
+                values = values_str.replace('\"None\"', 'NULL')
+                sql = "REPLACE INTO %s (%s) VALUES (%s);" % (table_name, columns, values)
+                print(sql)
+            else:
+                sql = "INSERT INTO " + table_name + " (" + headers_str + ") VALUES (" + values_str + ");"
+
+            i += 1
+
+            try:
+                with connection.cursor() as cursor:
+                    # cursor.execute(sql)
+                    cursor.execute(sql)
+                    connection.commit()  # commit the changes
+            except Exception as e:
+                print(f'This is error: {e}')
+                error = str(e)
+                connection.rollback()  # rollback if any exception occurred (optional)
+                # write log to file
+                with open('log.txt', 'a') as f:
+                    current_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                    f.write(f'{current_date} {error} \n')
+                return {"detail": error}
+
+        with open('log.txt', 'a') as f:
+            current_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            f.write(f'{current_date} Insert {i} rows into {hoscode} success \n')
+
+        connection.close()
+
+        # url = "https://notify-api.line.me/api/notify"
+        #
+        # message = "API " + api_name + " end process"
+
+        # payload = f'message={message}'
+        # headers = {
+        #     'Authorization': 'Bearer ' + config_env["LINE_TOKEN"],
+        #     'Content-Type': 'application/x-www-form-urlencoded'
+        # }
+        #
+        # requests.request("POST", url, headers=headers, data=payload)
+
+        return {"detail": "Insert " + str(i) + " rows into " + hoscode + " success"}
 
 
 # api caller # ตัวเรียกข้อมูล

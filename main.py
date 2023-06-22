efff54dc395665df5d8cfec04b467c717ed3aaf5
yuinakorn <yuinakorn@hotmail.com>
2023-06-22 20:44:18 +0700

    test data

--- main.py
+++ main.py
@@ -74,9 +74,7 @@ async def receiver2(api_name: str, request: Request = Body(..., max_size=2000000
     print(
         "start import api_name: " + api_name + "\n" + "start_time = " + datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S"))
     json_data = await request.json()
-    payload = json.dumps(json_data)
-    print("payload: " + payload)
-    # data = json_data["data"]
+    print("data = ", json_data["data"])
     if api_name in outer_api_list:
         select_api(api_name, request)
     elif json_data["data"] is None:

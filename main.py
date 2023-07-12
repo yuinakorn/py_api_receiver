03620878500a2d8840b1857085e2a54a1dc4a004
yuinakorn <yuinakorn@hotmail.com>
2023-07-12 14:36:19 +0700

    update function select api uppercase test6

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def receiver2(api_name: str, request: Request = Body(..., max_size=2000000
 
     if api_name in outer_api_list:
         print("if statement api_name: " + api_name)
-        await select_api(api_name, json_data)
+        select_api(api_name, json_data)
     elif json_data["data"] is None or json_data["data"] == [] or json_data["data"] == "":
         print("data is None")
         return {"message": "data is None"}

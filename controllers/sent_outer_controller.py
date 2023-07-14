11cce55a15f9a2565671318eae758f3e75f256e8
yuinakorn <yuinakorn@hotmail.com>
2023-07-14 14:54:27 +0700

    test send cmu2

--- controllers/sent_outer_controller.py
+++ controllers/sent_outer_controller.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # api_url = config_env["API_URL"]
 
 def select_api(api_name: str, json_data):
-    api_url = None
+    global api_url
     print("inside select_api")
     upper_api_name = api_name.upper()
 
